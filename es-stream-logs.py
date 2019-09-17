fc6635e8a4149b4151a19ee8b08ad90adea13583
Lu Stadler <luc@spreadshirt.net>
2019-09-17 11:55:49 +0200

    Reformat code for less warnings and slightly better layout
    E.g. the core query function is not nested in the HTTP handler anymore.
    So it should be usable separately as well.

--- es-stream-logs.py
+++ es-stream-logs.py
@@ -108,72 +108,70 @@ def filter_dict(source, fields):
             pass
     return res
 
-# curl 'http://kibana-dc1.example.com/elasticsearch/_msearch' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0' -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: http://kibana-dc1.example.com/app/kibana' -H 'content-type: application/x-ndjson' -H 'kbn-version: 5.6.9' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' --data $'{"index":["application-2019.02.21"],"ignore_unavailable":true,"preference":1550757631050}\n{"version":true,"size":500,"sort":[{"@timestamp":{"order":"desc","unmapped_type":"boolean"}}],"query":{"bool":{"must":[{"match_all":{}},{"match_phrase":{"level":{"query":"ERROR"}}},{"match_phrase":{"application_name":{"query":"api"}}},{"range":{"@timestamp":{"gte":1550757641281,"lte":1550758541281,"format":"epoch_millis"}}}],"must_not":[]}},"_source":{"excludes":[]},"aggs":{"2":{"date_histogram":{"field":"@timestamp","interval":"30s","time_zone":"UTC","min_doc_count":1}}},"stored_fields":["*"],"script_fields":{},"docvalue_fields":["@timestamp","time"],"highlight":{"pre_tags":["@kibana-highlighted-field@"],"post_tags":["@/kibana-highlighted-field@"],"fields":{"*":{"highlight_query":{"bool":{"must":[{"match_all":{}},{"match_phrase":{"level":{"query":"ERROR"}}},{"match_phrase":{"application_name":{"query":"api"}}},{"range":{"@timestamp":{"gte":1550757641281,"lte":1550758541281,"format":"epoch_millis"}}}],"must_not":[]}}}},"fragment_size":2147483647}}\n'
-@APP.route('/logs')
-def stream_logs():
-    def now_ms():
-        return int(datetime.utcnow().timestamp()*1000)
+def stream_logs(es, dc='dc1', index="application-*", q=None, fmt="html", fields="all", separator=" ", **kwargs):
+    """ Contruct query and stream logs given the elasticsearch client and parameters. """
 
-    def results(es, dc='dc1', index="application-*", q=None, fmt="html", fields="all", separator=" ", **kwargs):
-        if fields != "all":
-            fields = fields.split(',')
+    if fields != "all":
+        fields = fields.split(',')
 
-        from_timestamp = kwargs.get("from", "now-5m")
-        to_timestamp = kwargs.get("to", "now")
-        # remove from and to because they are not fields, None to prevent KeyError
-        kwargs.pop("from", None)
-        kwargs.pop("to", None)
+    from_timestamp = kwargs.get("from", "now-5m")
+    to_timestamp = kwargs.get("to", "now")
+    # remove from and to because they are not fields, None to prevent KeyError
+    kwargs.pop("from", None)
+    kwargs.pop("to", None)
 
-        last_timestamp = from_timestamp
-        seen = {}
+    last_timestamp = from_timestamp
+    seen = {}
 
-        required_filters = []
-        excluded_filters = []
-        if q:
-            required_filters.append({"query_string": {"query": q, "analyze_wildcard": True}})
+    required_filters = []
+    excluded_filters = []
+    if q:
+        required_filters.append({"query_string": {"query": q, "analyze_wildcard": True}})
 
-        for key, val in kwargs.items():
-            if val == "":
-                if key.startswith("-"):
-                    excluded_filters.append({"exists": {"field": key[1:]}})
-                else:
-                    required_filters.append({"exists": {"field": key}})
+    for key, val in kwargs.items():
+        if val == "":
+            if key.startswith("-"):
+                excluded_filters.append({"exists": {"field": key[1:]}})
             else:
-                if "," in val:
-                    required_filters.append({"bool" : {"should": [{"term": {key: v}} for v in val.split(',')]}})
-                elif val.startswith("-"):
-                    excluded_filters.append({"term": {key: val[1:]}})
-                else:
-                    required_filters.append({"term": {key: val}})
+                required_filters.append({"exists": {"field": key}})
+        else:
+            if "," in val:
+                required_filters.append({"bool" : {
+                    "should": [{"term": {key: v}} for v in val.split(',')]
+                    }})
+            elif val.startswith("-"):
+                excluded_filters.append({"term": {key: val[1:]}})
+            else:
+                required_filters.append({"term": {key: val}})
 
 
-        # send something so we return an initial response
-        yield ""
+    # send something so we return an initial response
+    yield ""
 
-        if fmt == "html":
-            yield """<!doctype html>
+    if fmt == "html":
+        yield """<!doctype html>
 <html>
 <head>
-    <style>
-        table {
-            width: 100%;
-        }
+<style>
+    table {
+        width: 100%;
+    }
 
-        thead tr {
-            font-weight: bold;
-        }
+    thead tr {
+        font-weight: bold;
+    }
 
-        td {
-            margin-right: 1em;
-            border-bottom: 1px solid #ddd;
-            font-size: 14px;
-            font-family: monospace;
-            max-width: 30em;
-            word-wrap: break-word;
-            overflow-y: auto;
-            vertical-align: top;
-        }
-    </style>
+    td {
+        margin-right: 1em;
+        border-bottom: 1px solid #ddd;
+        font-size: 14px;
+        font-family: monospace;
+        max-width: 30em;
+        word-wrap: break-word;
+        overflow-y: auto;
+        vertical-align: top;
+    }
+</style>
 </head>
 <body>
 <table>
@@ -181,99 +179,101 @@ def stream_logs():
 <tr>
 """
 
-            if fields == "all":
-                fields = ["@timestamp", "hostname", "level", "message", "thread_name", "stack_trace"]
+        if fields == "all":
+            fields = ["@timestamp", "hostname", "level", "message", "thread_name", "stack_trace"]
 
-            for field in fields:
-                yield f"<td>{field}</td>"
+        for field in fields:
+            yield f"<td>{field}</td>"
 
-            yield """
+        yield """
 </tr>
 </thead>
 
 <tbody>
 """
 
-        while True:
-            now = now_ms()
-
-            timerange = {"range": {"@timestamp": {"gte": last_timestamp, "lt": to_timestamp}}}
-            try:
-                resp = es.search(index=index,
-                        body={
-                            "size": 500,
-                            "sort": [{"@timestamp":{"order": "asc"}}],
-                            "query": {
-                                "bool": {
-                                    "must": [*required_filters, timerange],
-                                    "must_not": excluded_filters
-                                    }
+    while True:
+        timerange = {"range": {"@timestamp": {"gte": last_timestamp, "lt": to_timestamp}}}
+        try:
+            resp = es.search(index=index,
+                    body={
+                        "size": 500,
+                        "sort": [{"@timestamp":{"order": "asc"}}],
+                        "query": {
+                            "bool": {
+                                "must": [*required_filters, timerange],
+                                "must_not": excluded_filters
                                 }
-                            })
-            except elasticsearch.ConnectionTimeout as ex:
-                print(ex)
-                time.sleep(1)
-                continue
-            except elasticsearch.AuthenticationException as ex:
-                yield str(ex)
-                return
-
-            last_seen = {}
-            i = 0
-            for hit in resp['hits']['hits']:
-                i += 1
-                source = hit['_source']
-                _id = hit['_id']
-                last_seen[_id] = True
-                if _id in seen:
-                    continue
-
-                yield "\n"
-
-                ts = int(datetime.strptime(source['@timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ').timestamp()*1000)
-                if isinstance(last_timestamp, str):
-                    last_timestamp = ts
-                else:
-                    last_timestamp = max(ts, last_timestamp)
-
-                if fmt == "json":
-                    if fields != "all":
-                        source = filter_dict(source, fields)
-                    yield json.dumps(source)
-                if fmt == "html":
-                    try:
-                        trace_id = nested_get(source, ["tracing", "trace_id"])
-                        trace_id_link = f"<a href=\"https://tracing.example.com/?traceId={trace_id}&dc={dc}\">{trace_id}</a>"
-                        source["tracing"]["trace_id"] = trace_id_link
-                    except KeyError:
-                        pass
-
-                    source = filter_dict(source, fields)
-                    yield "<tr>\n"
-                    for field in fields:
-                        yield f"    <td>{source.get(field, '')}</td>\n"
-                    yield "</tr>\n"
-                else:
-                    if fields != "all":
-                        source = filter_dict(source, fields)
-                        yield separator.join((str(val) for val in source.values()))
-                    else:
-                        try:
-                            hostname = source.get('hostname', source.get('HOSTNAME', '<no-hostname>'))
-                            yield f"{source['@timestamp']} -- {source['level']} [{hostname}] {source['message']}"
-                            if 'thread_name' in source:
-                                yield f": {source['thread_name']}"
-                            if 'stack_trace' in source:
-                                yield f"\n{source['stack_trace']}"
-                        except KeyError:
-                            yield str(source)
-            seen = last_seen
-
-            # print space to try and keep connection open
-            yield " "
-
+                            }
+                        })
+        except elasticsearch.ConnectionTimeout as ex:
+            print(ex)
             time.sleep(1)
+            continue
+        except elasticsearch.AuthenticationException as ex:
+            yield str(ex)
+            return
 
+        last_seen = {}
+        i = 0
+        for hit in resp['hits']['hits']:
+            i += 1
+            source = hit['_source']
+            _id = hit['_id']
+            last_seen[_id] = True
+            if _id in seen:
+                continue
+
+            yield "\n"
+
+            timestamp = int(datetime.strptime(source['@timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ').timestamp()*1000)
+            if isinstance(last_timestamp, str):
+                last_timestamp = timestamp
+            else:
+                last_timestamp = max(timestamp, last_timestamp)
+
+            if fmt == "json":
+                if fields != "all":
+                    source = filter_dict(source, fields)
+                yield json.dumps(source)
+            if fmt == "html":
+                try:
+                    trace_id = nested_get(source, ["tracing", "trace_id"])
+                    trace_id_link = f"<a href=\"https://tracing.example.com/?traceId={trace_id}&dc={dc}\">{trace_id}</a>"
+                    source["tracing"]["trace_id"] = trace_id_link
+                except KeyError:
+                    pass
+
+                source = filter_dict(source, fields)
+                yield "<tr>\n"
+                for field in fields:
+                    yield f"    <td>{source.get(field, '')}</td>\n"
+                yield "</tr>\n"
+            else:
+                if fields != "all":
+                    source = filter_dict(source, fields)
+                    yield separator.join((str(val) for val in source.values()))
+                else:
+                    try:
+                        hostname = source.get('hostname', source.get('HOSTNAME', '<no-hostname>'))
+                        yield f"{source['@timestamp']} -- {source['level']} [{hostname}] {source['message']}"
+                        if 'thread_name' in source:
+                            yield f": {source['thread_name']}"
+                        if 'stack_trace' in source:
+                            yield f"\n{source['stack_trace']}"
+                    except KeyError:
+                        yield str(source)
+        seen = last_seen
+
+        # print space to try and keep connection open
+        yield " "
+
+        time.sleep(1)
+
+# curl 'http://kibana-dc1.example.com/elasticsearch/_msearch' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0' -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: http://kibana-dc1.example.com/app/kibana' -H 'content-type: application/x-ndjson' -H 'kbn-version: 5.6.9' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' --data $'{"index":["application-2019.02.21"],"ignore_unavailable":true,"preference":1550757631050}\n{"version":true,"size":500,"sort":[{"@timestamp":{"order":"desc","unmapped_type":"boolean"}}],"query":{"bool":{"must":[{"match_all":{}},{"match_phrase":{"level":{"query":"ERROR"}}},{"match_phrase":{"application_name":{"query":"api"}}},{"range":{"@timestamp":{"gte":1550757641281,"lte":1550758541281,"format":"epoch_millis"}}}],"must_not":[]}},"_source":{"excludes":[]},"aggs":{"2":{"date_histogram":{"field":"@timestamp","interval":"30s","time_zone":"UTC","min_doc_count":1}}},"stored_fields":["*"],"script_fields":{},"docvalue_fields":["@timestamp","time"],"highlight":{"pre_tags":["@kibana-highlighted-field@"],"post_tags":["@/kibana-highlighted-field@"],"fields":{"*":{"highlight_query":{"bool":{"must":[{"match_all":{}},{"match_phrase":{"level":{"query":"ERROR"}}},{"match_phrase":{"application_name":{"query":"api"}}},{"range":{"@timestamp":{"gte":1550757641281,"lte":1550758541281,"format":"epoch_millis"}}}],"must_not":[]}}}},"fragment_size":2147483647}}\n'
+@APP.route('/logs')
+def serve_logs():
+    """ Serve logs. """
     if ES_USER is None or ES_USER is None:
         if not request.authorization:
             return Response('Could not verify your access level for that URL.\n'
@@ -281,13 +281,13 @@ def stream_logs():
                             401,
                             {'WWW-Authenticate': 'Basic realm="Login with  LDAP credentials"'})
 
-    dc = request.args.get('dc') or 'dc1'
-    if dc not in DATACENTERS:
-        abort(400, f"unknown datacenter '{dc}'")
+    datacenter = request.args.get('dc') or 'dc1'
+    if datacenter not in DATACENTERS:
+        abort(400, f"unknown datacenter '{datacenter}'")
 
-    es = Elasticsearch([f"https://es-log-{dc}.example.com:443"],
-                       http_auth=(ES_USER or request.authorization.username,
-                                  ES_PASSWORD or request.authorization.password))
+    es_client = Elasticsearch([f"https://es-log-{datacenter}.example.com:443"],
+                              http_auth=(ES_USER or request.authorization.username,
+                                         ES_PASSWORD or request.authorization.password))
 
     fmt = request.args.get("fmt", "html")
     content_type = "text/html"
@@ -296,9 +296,12 @@ def stream_logs():
     elif fmt == "text":
         content_type = "text/plain"
 
-    return Response(results(es, **request.args), content_type=content_type+'; charset=utf-8')
+    return Response(stream_logs(es_client, **request.args),
+                    content_type=content_type+'; charset=utf-8')
 
-if __name__ == "__main__":
+def run_app():
+    """ Run application. """
+
     host = 'localhost'
     port = 3028
     if len(sys.argv) > 1:
@@ -306,3 +309,6 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         port = int(sys.argv[2])
     APP.run(host=host, port=port, threaded=True)
+
+if __name__ == "__main__":
+    run_app()
