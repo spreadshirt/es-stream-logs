1e888e7746cb54bb62a32bd1cb855d893fa939d5
Andreas Linz <anli@spreadshirt.net>
2019-12-16 17:29:34 +0100

    Escape query string in title

--- es-stream-logs.py
+++ es-stream-logs.py
@@ -273,7 +273,7 @@ def aggregation(es, query: Query):
     template = Template(r"""<?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg" class="chart" width="{{ width }}" height="{{ height }}" xmlns:xlink="http://www.w3.org/1999/xlink">
 
-<title id="title">Aggregation for query: {{ query_str }}</title>
+<title id="title">Aggregation for query: {{ query_str | e }}</title>
 <style>
 svg {
     font-family: monospace;
