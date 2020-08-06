134cc59b7efa12fb9c9ff872b57c11af0ed34f5e
Lu Stadler <luc@spreadshirt.net>
2020-08-06 16:22:52 +0200

    Fix query tests

--- test_query.py
+++ test_query.py
@@ -8,8 +8,8 @@ class QueryTest(unittest.TestCase):
     """ Test query class. """
 
     def setUp(self):
-        self.config = Config(default_endpoint='default', endpoints=[],
-                             field_format={}, default_fields={})
+        self.config = Config(default_endpoint='default', endpoints=[], indices=[],
+                             field_format={}, default_fields={}, queries=[])
 
     def test_parse_defaults(self):
         """ Test defaults. """
@@ -30,16 +30,16 @@ class QueryTest(unittest.TestCase):
         """ Test url conversion. """
 
         query = Query(self.config)
-        self.assertEqual(query.as_url('/'), '/?dc=default&index=application-*&from=now-5m&to=now&interval=auto')
+        self.assertEqual(query.as_url('/'), '/?dc=default&index=application-%2A&from=now-15m&to=now&interval=auto')
 
         query = Query(self.config, level='WARN')
-        self.assertEqual(query.as_url('/'), '/?dc=default&index=application-*&from=now-5m&to=now&interval=auto&level=WARN')
+        self.assertEqual(query.as_url('/'), '/?dc=default&index=application-%2A&from=now-15m&to=now&interval=auto&level=WARN')
 
     def assert_defaults(self, query, args=None):
         """ Assert query params. """
         self.assertEqual(query.datacenter, 'default')
         self.assertEqual(query.index, 'application-*')
-        self.assertEqual(query.from_timestamp, 'now-5m')
+        self.assertEqual(query.from_timestamp, 'now-15m')
         self.assertEqual(query.to_timestamp, 'now')
         self.assertEqual(query.query_string, None)
         self.assertEqual(query.fields, None)
