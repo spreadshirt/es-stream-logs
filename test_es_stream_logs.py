4ae403265b80637995a7fab914d72eb63f590197
Luna Stadler <luc@spreadshirt.net>
2023-02-19 14:27:04 +0100

    Fix linting errors

--- test_es_stream_logs.py
+++ test_es_stream_logs.py
@@ -3,6 +3,7 @@ import unittest
 
 from es_stream_logs import parse_timestamp
 
+
 class ParseTimestampTestCase(unittest.TestCase):
     def test_full(self):
         self.assertEqual(0, parse_timestamp("1970-01-01T00:00:00Z"))
@@ -15,7 +16,7 @@ class ParseTimestampTestCase(unittest.TestCase):
     def test_relative_minutes(self):
         now = time.time()
         times = ["now", "now-1m", "now-10m", "now-30m", "now-90m"]
-        offsets = [0, 60, 60*10, 60*30, 60*90]
+        offsets = [0, 60, 60 * 10, 60 * 30, 60 * 90]
         for i, t in enumerate(times):
             with self.subTest(t):
                 self.assertTrue(abs((now - offsets[i]) - parse_timestamp(t)) < 0.1)
@@ -23,7 +24,7 @@ class ParseTimestampTestCase(unittest.TestCase):
     def test_relative_hours(self):
         now = time.time()
         times = ["now", "now-1h", "now-3h", "now-10h", "now-48h"]
-        offsets = [0, 1*60*60, 3*60*60, 10*60*60, 48*60*60]
+        offsets = [0, 1 * 60 * 60, 3 * 60 * 60, 10 * 60 * 60, 48 * 60 * 60]
         for i, t in enumerate(times):
             with self.subTest(t):
                 self.assertTrue(abs((now - offsets[i]) - parse_timestamp(t)) < 0.1)
@@ -31,7 +32,11 @@ class ParseTimestampTestCase(unittest.TestCase):
     def test_relative_days(self):
         now = time.time()
         times = ["now", "now-1d", "now-3d", "now-14d", "now-40d"]
-        offsets = [0, 1*24*60*60, 3*24*60*60, 14*24*60*60, 40*24*60*60]
+        offsets = [0,
+                   1 * 24 * 60 * 60,
+                   3 * 24 * 60 * 60,
+                   14 * 24 * 60 * 60,
+                   40 * 24 * 60 * 60]
         for i, t in enumerate(times):
             with self.subTest(t):
                 self.assertTrue(abs((now - offsets[i]) - parse_timestamp(t)) < 0.1)
