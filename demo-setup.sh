3f1de0be7b7e2c4dac530ebfb1304e7ce0188a0d
Luna Stadler <luc@spreadshirt.net>
2026-07-07 14:55:09 +0200

    Add example for nested objects in arrays
    Basically a simple test case for 4604cf0.

--- demo-setup.sh
+++ demo-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl localhost:9200/logs-default-pipeline/_doc -H 'Content-Type: application/json' -d'{"@timestamp":"'$(date --utc '+%Y-%m-%dT%H:%M:%SZ')'", "application_name": "test", "hostname": "'$(hostname)'", "level": "INFO", "message": "hi!", "tracing": {"trace_id": "'$RANDOM'"}}'
+curl localhost:9200/logs-default-pipeline/_doc -H 'Content-Type: application/json' -d'{"@timestamp":"'$(date --utc '+%Y-%m-%dT%H:%M:%SZ')'", "application_name": "test", "hostname": "'$(hostname)'", "level": "INFO", "message": "hi!", "tags": [{"nested": "yes"}], "tracing": {"trace_id": "'$RANDOM'"}}'
